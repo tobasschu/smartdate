e428aab48626acc93ef5a310db6791c05c26645e
Tobias Schumacher <tobias@tschumacher.de>
2018-01-18 18:33:53 +0100

    * license update

--- src/test/java/de/tschumacher/smartdate/parser/DailySmartDateParserItemTest.java
+++ src/test/java/de/tschumacher/smartdate/parser/DailySmartDateParserItemTest.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2017 Tobias Schumacher
- * 
+ * Copyright 2018 Tobias Schumacher
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -27,7 +27,7 @@ import de.tschumacher.smartdate.domain.SmartDate;
 public class DailySmartDateParserItemTest {
 
   private static String[] QUERIES = {"15.02.2012", "Feb 15, 2012", "02-15-2012", "Feb 15 2012",
-      "February 15, 2012", "February 15 2012", "15. Feb 2012", "15. February 2012"};
+    "February 15, 2012", "February 15 2012", "15. Feb 2012", "15. February 2012"};
 
   private DailySmartDateParserItem parser;
 
